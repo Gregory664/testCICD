55ab586af559bf4f23092066419d37368705a1ec
Gregory Belevskiy <gbelevskii@neoflex.ru>
2021-05-29 12:55:12 +0300

    some change 3

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -2,5 +2,6 @@ public class Solution {
     public static void main(String[] args) {
         System.out.println(Calculator.sum(2,2));
         System.out.println(Calculator.sum(2,2));
+        System.out.println(Calculator.sum(2,2));
     }
 }
